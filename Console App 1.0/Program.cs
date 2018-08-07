60209a7ca01fc847344bd472db2d21f75c656c61
Prince Waichal <princeironman@gmail.com>
2018-08-07 13:05:16 +0530

    Improvement from If_Else to Switch Case

--- Console App 1.0/Program.cs
+++ Console App 1.0/Program.cs
@@ -24,7 +24,7 @@ namespace Console_App_1._0
             }
 
         }
-        
+      
         public static void Add()
             // Function for Addition of Two Integers
         {
@@ -91,19 +91,32 @@ namespace Console_App_1._0
             Console.Clear();
             Console.WriteLine("\nEnter the following details :\n");
             Console.WriteLine("\nEnter Marks for 1st Subject : \t");
-            long s1 = Convert.ToInt64(Console.ReadLine());
+            double s1 = Convert.ToInt64(Console.ReadLine());
             Console.WriteLine("\nEnter Marks for 2nd Subject : \t");
-            long s2 = Convert.ToInt64(Console.ReadLine());
+            double s2 = Convert.ToInt64(Console.ReadLine());
             Console.WriteLine("\nEnter Marks for 3rd Subject : \t");
-            long s3 = Convert.ToInt64(Console.ReadLine());
+            double s3 = Convert.ToInt64(Console.ReadLine());
             Console.WriteLine("\nEnter Marks for 4th Subject : \t");
-            long s4 = Convert.ToInt64(Console.ReadLine());
+            double s4 = Convert.ToInt64(Console.ReadLine());
             Console.WriteLine("\nEnter Marks for 5th Subject : \t");
-            long s5 = Convert.ToInt64(Console.ReadLine());
+            double s5 = Convert.ToInt64(Console.ReadLine());
             Console.WriteLine("\nEnter Marks for 6th Subject : \t");
-            long s6 = Convert.ToInt64(Console.ReadLine());
-            long total = s1 + s2 + s3 + s4 + s5 + s6;
-            Console.WriteLine("\nTotal Marks Secured : \t");
+            double s6 = Convert.ToInt64(Console.ReadLine());
+            double total = s1 + s2 + s3 + s4 + s5 + s6;
+            double per = total / 6;
+            Console.WriteLine("\nTotal Marks Secured : \t{0} and percntage are \t{1} %",total.ToString(),per.ToString());
+            Console.ReadLine();
+        }
+
+        public static void Area()
+        //Calculating Area of Circle
+        {
+            Console.Clear();
+            const double pi = 3.14;
+            Console.WriteLine("\nEnter the Radius of the Circle");
+            double r = Convert.ToDouble(Console.ReadLine());
+            double a = pi * r * r;
+            Console.WriteLine("\nThe Radius give is {0} and the area of the circle therefore is\n{1}",r.ToString(),a.ToString());
             Console.ReadLine();
         }
 
@@ -116,35 +129,43 @@ namespace Console_App_1._0
             Console.WriteLine("\n_________________________________________");
             Console.WriteLine("\n1. Addition of Two Numbers\n\n2. Subtraction of Two Numbers");
             Console.WriteLine("\n3. Multiplication of Two Numbers\n\n4. Division of Two Numbers");
+            Console.WriteLine("\n5. Area of a Circle\n\n6. Percentage of a Student");
             Console.WriteLine("\nPress Any Other Key to Exit the Console Application.");
-            char a = Convert.ToChar(Console.ReadLine());
+            int a = Convert.ToInt32(Console.ReadLine());
 
-            if (a >= 1 && a <= 10)
+            switch (a)
             {
-                if (a == 1)
-                {
+                case 1:
                     Add();
-                }
-                if (a == 2)
-                {
+                    break;
+                case 2:
                     Sub();
-                }
-                if (a == 3)
-                {
+                    break;
+                case 3:
                     Multiply();
-                }
-                if (a == 4)
-                {
+                    break;
+                case 4:
                     Division();
-                }
-                else
-                {
-                    Console.WriteLine("Please select the only number which is provided in the list");
-                }
-            }
-            else
-            {
-                Exit();
+                    break;
+                case 5:
+                    Area();
+                    break;
+                case 6:
+                    Student();
+                    break;
+
+                default:
+                    Console.WriteLine("\nWhat you wish to do?\na. Continue the Program\nb. Exit the Program");
+                    char c = Convert.ToChar(Console.ReadLine());
+                    if(c=='a'||c=='A')
+                    {
+                        Main();
+                    }
+                    else
+                    {
+                        Exit();
+                    }                  
+                    break;
             }
         }
     }
